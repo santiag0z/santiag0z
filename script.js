8687b9f63df02c2c603aeaee81d832cf04043c73
santiag0z <santiagoz.silvz@gmail.com>
2025-06-21 13:56:33 -0300

    feat: Implement typing effect on description using JavaScript

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("typing-effect");
+  const textToType = "Network Analyst...";
+  let charIndex = 0;
+  const typingSpeed = 100;
+  const eraseSpeed = 50;
+  const newTextDelay = 2000;
+
+  const phrases = [
+    "Network Analyst...",
+    "Contributor LibreNMS...",
+    "Technologist in Systems Analysis and Development...",
+  ];
+  let phraseIndex = 0;
+
+  function type() {
+    if (charIndex < phrases[phraseIndex].length) {
+      textElement.textContent += phrases[phraseIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingSpeed);
+    } else {
+      if (phrases.length > 1) {
+        setTimeout(erase, newTextDelay);
+      }
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      textElement.textContent = phrases[phraseIndex].substring(
+        0,
+        charIndex - 1
+      );
+      charIndex--;
+      setTimeout(erase, eraseSpeed);
+    } else {
+      phraseIndex++;
+      if (phraseIndex >= phrases.length) {
+        phraseIndex = 0;
+      }
+      setTimeout(type, typingSpeed + 500);
+    }
+  }
+
+  type();
+});
